d65aba6aa46a1f2d560866fdbfe853cf64b9a72d
Gabriel de Amorim <gdias0568@gmail.com>
2024-04-04 15:02:55 -0300

    Simplificando a sintaxe do código javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,41 @@
 
+const offSet = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offSet}&limit=${limit}`;
 
 
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="name">${pokemon.name}</span>
+            <span class="number">#001</span>
 
-pokeApi.getPokemons().then((pokemonList) => {
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonsList = document.getElementById('pokemonsList');
+
+
+pokeApi.getPokemons().then((pokemons = []) => {
+
+    const newList = pokemons.map((pokemon) => convertPokemonToLi(pokemon))
+
+    const newHtml = newList.join('');
+
+    pokemonsList.innerHTML += newHtml;
+
+})
+
+
     
-    const listItems = []
 
-        for (let i = 0; i < pokemonList.length; i++) {
-            const pokemon = pokemonList[i];
-            listItems.push(convertPokemonToLi(pokemon))
-        }
-        console.log(listItems);
-    })
-
-
-    const pokemonsList = document.getElementById('pokemonsList')
-
-    function convertPokemonToLi(pokemon) {
-        return `
-            <li class="pokemon">
-                <span class="name">${pokemon.name}</span>
-                <span class="number">#001</span>
-
-                <div class="detail">
-                    <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
-                    </ol>
-
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}">
-                </div>
-            </li>
-        `
-    }
+    
